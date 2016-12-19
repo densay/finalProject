364fde80f21832e39e3645ca509816e1b12b140d
Katherine Gao <kgao@wellesley.edu>
2016-12-19 17:20:29 -0500

    Add files via upload

---
+++ AllCourses.java
@@ -0,0 +1,114 @@
+/** Katherine Gao, Xochitl Say, Hannah Weismann
+  * 
+  * CS 230 Final Project
+  * 
+  * 12/14/16
+  * 
+  * FinalGUI.java
+  */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+
+public class AllCourses extends JPanel {
+  
+  private JButton nextButton;
+  private JPanel questionPanel;
+  private JPanel buttonPanel;
+  private JLabel question1;
+  private JLabel question2;
+  private JLabel question3;
+  private LinkedList<String> completedCourses;
+  private CourseNamePanel oneHundred;
+  private CourseNamePanel twoHundred;
+  private CourseNamePanel threeHundred;
+  
+  public AllCourses(String one, String two, String three) {
+    // set layout of the panel:
+    setLayout(new BorderLayout());
+    oneHundred = new CourseNamePanel(one);
+    oneHundred.setPreferredSize(new Dimension(400,200));
+    twoHundred = new CourseNamePanel(two);
+    twoHundred.setPreferredSize(new Dimension(400,200));
+    threeHundred = new CourseNamePanel(three);
+    threeHundred.setPreferredSize(new Dimension(400,200));
+    
+    questionPanel = new JPanel();
+    questionPanel.setPreferredSize(new Dimension(50, 50));
+    questionPanel.setLayout(new BorderLayout());
+    question1 = new JLabel("How many 100 level classes have you taken?");
+    question2 = new JLabel("How many 200 level classes have you taken?", question2.CENTER);
+    question3 = new JLabel("How many 300 level classes have you taken?", question3.CENTER);
+    questionPanel.add(question1, BorderLayout.LINE_START);
+    questionPanel.add(question2, BorderLayout.CENTER);
+    questionPanel.add(question3, BorderLayout.EAST);
+    
+    buttonPanel = new JPanel();
+    buttonPanel.setPreferredSize(new Dimension(25,40));
+    nextButton = new JButton("Next");
+    nextButton.addActionListener(new ButtonListener());
+    buttonPanel.add(nextButton);
+    
+    add(oneHundred, BorderLayout.WEST);
+    add(twoHundred, BorderLayout.CENTER);
+    add(threeHundred, BorderLayout.EAST);
+    add(questionPanel, BorderLayout.NORTH);
+    add(buttonPanel, BorderLayout.SOUTH);
+  }
+  
+  private class ButtonListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+      completedCourses = new LinkedList<String>();
+      if (event.getSource() == nextButton) {
+        LinkedList<String> first = oneHundred.getTakenCourses();
+        LinkedList<String> second = twoHundred.getTakenCourses();
+        LinkedList<String> third = threeHundred.getTakenCourses();
+        for (int i=0; i<first.size(); i++){
+          String class1 = first.get(i);
+          String [] class1Array = class1.split(":");
+          completedCourses.add(class1Array[0]);
+        }
+        for (int j=0; j<second.size(); j++) {
+          String class2 = second.get(j);
+          String [] class2Array = class2.split(":");
+          completedCourses.add(class2Array[0]);
+        }
+        for (int k=0; k<third.size(); k++) {
+          String class3 = third.get(k);
+          String [] class3Array = class3.split(":");
+          completedCourses.add(class3Array[0]);
+        }
+        for (int t=0; t<completedCourses.size(); t++) {
+        System.out.println(completedCourses.get(t));
+        }
+      }
+      // Write information into text file to be used in other classes
+      writeToFile(completedCourses, "SelectedCourses.txt");
+    }
+  }
+  
+  private void writeToFile(LinkedList<String> list, String outputName) {
+    try {
+      PrintWriter outFile = new PrintWriter(new File(outputName));
+      for (int i=0; i<list.size(); i++) {
+        outFile.println(list.get(i));
+      }
+      outFile.close();
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+  }
+  
+  public static void main(String [] args) {
+    AllCourses allCourses = new AllCourses("ALL100.txt", "ALL200.txt", "ALL300.txt"); 
+    JFrame frame = new JFrame("Choose your Major");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(allCourses);
+    frame.pack();
+    frame.setSize(1200, 600);
+    frame.setVisible(true);
+  }
+}
