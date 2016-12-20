c69b7e98b739d9e667b5e599678d899cfe265b99
densay <xochitlsay@gmail.com>
2016-12-19 22:39:53 -0500

    Update CourseTrajectories.java

--- CourseTrajectories.java
+++ CourseTrajectories.java
@@ -1,121 +1,94 @@
+
+/**
+ * This class is used to set up different major dependencies
+ * Each graph represents course dependencies
+ * 
+ * 
+ * */
+
 import java.io.*;
 import java.util.*;
-import java.util.Scanner;
 import javafoundations.*;
 
 public class CourseTrajectories<T> {
   AdjMatGraphPlus<String> graph;
 
-
+  
   public CourseTrajectories(String tgfFile) throws FileNotFoundException {
     graph = new AdjMatGraphPlus<String>();
-    //tgfFile = (T)tgfFile;
     graph = graph.fromTGF(tgfFile);
 
   }
     
-   /** Returns true if this graph is empty, false otherwise. */
+   /*
+   * just checks if the major dependency is populated
+   * @return boolean true or false if graph is full or empty
+   * */
   public boolean isEmpty(){
    return graph.isEmpty(); 
   }
    
-   /** Returns the number of vertices in this graph. */
-  public int numberOfCourses() {
-    return graph.n();
-  }
-
-   /** Returns the number of arcs in this graph. */
-  public int numOfDependecies() {
-    return graph.m();
-  }
-   
-   /** Returns true iff a directed edge exists b/w given vertices */
-  public boolean isArc(String vertex1, String vertex2) {
-    return graph.isArc(vertex1, vertex2);
-  }
-     
-   /** Adds a vertex to this graph, associating object with vertex.
-   * If the vertex already exists, nothing is inserted. */
+     /*
+   * method used to add courses to major depedency graph 
+   * uses adj mat graph 
+   * @param vertex takes in the name of the course
+   * 
+   * 
+   * */
   public void addVertex (String vertex) {
     graph.addVertex(vertex);
   }
 
-   /** Removes a single vertex with the given value from this graph.
-   * If the vertex does not exist, it does not change the graph. */
+    
+  /*
+   * method takes a course away 
+   * @param takes in the name of the image 
+   * 
+   * */
   public void removeVertex (String vertex) {
     graph.removeVertex(vertex);
   }
 
-   /** Inserts an arc between two vertices of this graph,
-   * if the vertices exist. Else it does not change the graph. */
-  public void addArc (String vertex1, String vertex2) {
-    graph.addArc(vertex1, vertex2);
-  }
-
-   /** Removes an arc between two vertices of this graph,
-   * if the vertices exist. Else it does not change the graph. */
-   public void removeArc (String vertex1, String vertex2){
-    graph.removeArc(vertex1, vertex2);
-  }
-   
-   /** Inserts an edge between two vertices of this graph,
-   * if the vertices exist. Else does not change the graph. */
-   public void addEdge (String vertex1, String vertex2) {
-    graph.addEdge(vertex1, vertex2);
-  }
-
-   /** Removes an edge between two vertices of this graph,
-   if the vertices exist, else does not change the graph. */
-   public void removeEdge (String vertex1, String vertex2) {
-    graph.removeEdge(vertex1, vertex2);
-  }
-     
-     
-   /** Retrieve from a graph the vertices adjacent to vertex v.
-   Assume that the vertex is in the graph */
+    /*
+   * @param vertex takes in the name of vertex to find
+   * @return the nodes that it points to in the graph
+   * */
    public LinkedList<String> getSuccessors(String vertex) {
      return graph.getSuccessors(vertex);
    }
-        
-
-  /** Retrieve from a graph the vertices x preceding vertex v (x->v)
- and returns them onto a linked list */
+ /*
+   * @param vertex takes in the name of vertex to find what comes before it 
+   * @return the nodes that point to in the graph
+   * */
    public LinkedList<String> getPredecessors(String vertex) {
      return graph.getPredecessors(vertex);
    }
    
-   public LinkedList<String> listByPriority() throws GraphCycleException{
-    return graph.listByPriority(); 
-   }
-   public LinkedList<String> dfsTraversal(String startVertex){
-    return graph.dfsTraversal(startVertex); 
-   }
-   public LinkedList<String> bfsTraversal(String startVertex){
-    return graph.bfsTraversal(startVertex); 
-   }
-   
+  
    public AdjMatGraphPlus<String> fromTGF(String tgfFile) throws FileNotFoundException {
      return graph.fromTGF(tgfFile);
    }
-
+/***
+     * 
+     * 
+     * @return boolean based off of it it finds the course in the graph
+     * */
    public boolean containsCourse(String vertex) {
      return graph.containsVertex(vertex);
    }
-
-   //ALL SOURCES
+/*
+    * method used to find courses without prerequisites
+    * @returns linkedList of all courses
+    * */
   public LinkedList<String> allSources() {
     return graph.allSources();
+    }
+      /** @return a string representation of graph  */
+    public String toString() {
+
+      return graph.toString();
+
   }
-   /** Returns a string representation of the adjacency matrix. */
-  public String toString() {
-    
-    return graph.toString();
-          
-  }
-  
-  public boolean isSource(String vertex) {
-   return graph.isSource(vertex); 
-  }
-  
+ 
 
 }
